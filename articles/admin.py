12487652a4054e7e94f25c314764bab50fb5d61c
GillsEffect <mustafag0000@gmail.com>
2019-07-04 00:19:52 +0300

    FirstCommit

---
+++ articles/admin.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+from .models import Article
+from django.contrib import admin
+
+admin.site.register(Article)
+# Register your models here.
